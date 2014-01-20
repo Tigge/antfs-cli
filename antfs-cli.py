6038b077f5724b4f94c0c6695b446c9c8bc92ab4
Gustav Tiger <gustav@tiger.name>
2014-01-20 15:27:06 +0100

    Add main program

---
+++ antfs-cli.py
@@ -0,0 +1,311 @@
+#!/usr/bin/python
+#
+# Ant
+#
+# Copyright (c) 2012, Gustav Tiger <gustav@tiger.name>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+#from ant.base import Message
+#from ant.easy.node import Node, Message
+#from ant.easy.channel import Channel
+from ant.fs.manager import Application, AntFSAuthenticationException
+from ant.fs.file import File
+
+import utilities
+
+import array
+import logging
+import os
+import struct
+import sys
+import traceback
+
+_logger = logging.getLogger()
+
+_directories = {
+    ".":          File.Identifier.DEVICE,
+    "activities": File.Identifier.ACTIVITY,
+    "courses":    File.Identifier.COURSE,
+    #"profile":   File.Identifier.?
+    #"goals?":    File.Identifier.GOALS,
+    #"bloodprs":  File.Identifier.BLOOD_PRESSURE,
+    #"summaries": File.Identifier.ACTIVITY_SUMMARY,
+    "settings":   File.Identifier.SETTING,
+    "sports":     File.Identifier.SPORT_SETTING,
+    "totals":     File.Identifier.TOTALS,
+    "weight":     File.Identifier.WEIGHT,
+    "workouts":   File.Identifier.WORKOUT}
+
+_filetypes = dict((v, k) for (k, v) in _directories.items())
+
+
+class Device:
+    
+    class ProfileVersionException(Exception):
+        pass
+    
+    _PROFILE_VERSION      = 1
+    _PROFILE_VERSION_FILE = "profile_version"
+    
+    def __init__(self, basedir, serial, name):
+        self._path   = os.path.join(basedir, str(serial))
+        self._serial = serial
+        self._name   = name
+        
+        # Check profile version, if not a new device
+        if os.path.isdir(self._path):
+            if self.get_profile_version() < self._PROFILE_VERSION:
+                raise Device.ProfileVersionException("Profile version mismatch, too old")
+            elif self.get_profile_version() > self._PROFILE_VERSION:
+                raise Device.ProfileVersionException("Profile version mismatch, to new")
+
+        # Create directories
+        utilities.makedirs_if_not_exists(self._path)
+        for directory in _directories:
+            directory_path = os.path.join(self._path, directory)
+            utilities.makedirs_if_not_exists(directory_path)
+
+        # Write profile version (If none)
+        path = os.path.join(self._path, self._PROFILE_VERSION_FILE)
+        if not os.path.exists(path):
+            with open(path, 'wb') as f:
+                f.write(str(self._PROFILE_VERSION))
+
+    def get_path(self):
+        return self._path
+
+    def get_serial(self):
+        return self._serial
+
+    def get_name(self):
+        return self._name
+
+    def get_profile_version(self):
+        path = os.path.join(self._path, self._PROFILE_VERSION_FILE)
+        try:
+            with open(path, 'rb') as f:
+                return int(f.read())
+        except IOError as e:
+            # TODO
+            return 0
+
+    def read_passkey(self):
+
+        try:
+            with open(os.path.join(self._path, "authfile"), 'rb') as f:
+                d = array.array('B', f.read())
+                _logger.debug("loaded authfile: %r", d)
+                return d
+        except:
+            return None
+            
+    def write_passkey(self, passkey):
+
+        with open(os.path.join(self._path, "authfile"), 'wb') as f:
+            passkey.tofile(f)
+            _logger.debug("wrote authfile: %r, %r", self._serial, passkey)
+
+
+
+
+class AntFSCLI(Application):
+
+    PRODUCT_NAME = "antfs-cli"
+
+    def __init__(self):
+        Application.__init__(self)
+        
+        _logger.debug("Creating directories")
+        self.config_dir = utilities.XDG(self.PRODUCT_NAME).get_config_dir()
+        utilities.makedirs_if_not_exists(self.config_dir)
+        
+        self.device = None
+
+    def setup_channel(self, channel):
+        channel.set_period(4096)
+        channel.set_search_timeout(255)
+        channel.set_rf_freq(50)
+        channel.set_search_waveform([0x53, 0x00])
+        channel.set_id(0, 0x01, 0)
+        
+        channel.open()
+        #channel.request_message(Message.ID.RESPONSE_CHANNEL_STATUS)
+        print "Searching..."
+
+    def on_link(self, beacon):
+        _logger.debug("on link, %r, %r", beacon.get_serial(),
+                      beacon.get_descriptor())
+        self.link()
+        return True
+
+    def on_authentication(self, beacon):
+        _logger.debug("on authentication")
+        serial, name = self.authentication_serial()
+        self._device = Device(self.config_dir, serial, name)
+        
+        passkey = self._device.read_passkey()
+        print "Authenticating with", name, "(" + str(serial) + ")"
+        _logger.debug("serial %s, %r, %r", name, serial, passkey)
+        
+        if passkey != None:
+            try:
+                print " - Passkey:",
+                self.authentication_passkey(passkey)
+                print "OK"
+                return True
+            except AntFSAuthenticationException as e:
+                print "FAILED"
+                return False
+        else:
+            try:
+                print " - Pairing:",
+                passkey = self.authentication_pair(self.PRODUCT_NAME)
+                self._device.write_passkey(passkey)
+                print "OK"
+                return True
+            except AntFSAuthenticationException as e:
+                print "FAILED"
+                return False
+
+    def on_transport(self, beacon):
+
+        directory = self.download_directory()
+        directory.print_list()
+        
+        # Map local files to FIT file types
+        local_files  = {}
+        for folder, filetype in _directories.items():
+            local_files[filetype] = []
+            path = os.path.join(self._device.get_path(), folder)
+            for filename in os.listdir(path):
+                if os.path.splitext(filename)[1].lower() == ".fit":
+                    local_files[filetype] += [filename]
+
+        # Map remote files to FIT file types
+        remote_files = {}
+        for filetype in _filetypes:
+            remote_files[filetype] = []
+        for fil in directory.get_files():
+            if fil.get_fit_sub_type() in remote_files:
+                remote_files[fil.get_fit_sub_type()] += [fil]
+
+        # Calculate remote and local file diff
+        # TODO: rework when adding delete support
+        downloading, uploading, download_total, upload_total = {}, {}, 0, 0
+        for filetype in _filetypes:
+            downloading[filetype] = filter(lambda fil: self.get_filename(fil)
+                    not in local_files[filetype], remote_files[filetype])
+            download_total += len(downloading[filetype])
+            uploading[filetype] = filter(lambda name: name not in
+                    map(self.get_filename, remote_files[filetype]),
+                    local_files[filetype])
+            upload_total += len(uploading[filetype])
+
+        print "Downloading", download_total, "file(s)", \
+              "and uploading", upload_total, "file(s)"
+
+        # Download missing files:
+        for files in downloading.values():
+            for fileobject in files:
+                self.download_file(fileobject)
+
+        # Upload missing files:
+        if upload_total > 0:
+            # Upload
+            results = []
+            for typ, files in uploading.items():
+                for filename in files:
+                    print "upload", typ, filename
+                    index = self.upload_file(typ, filename)
+                    results[index] = (filename, typ)
+            # Rename
+            directory = self.download_directory()
+            for index, (filename, typ) in results:
+                file_object = filter(lambda f: f.get_index() == index,
+                        directory.get_files())[0]
+                src = os.path.join(self._device.get_path(), _filetypes[typ], filename)
+                dst = self.get_filepath(file_object)
+                print "move from", src, "to", dst
+                os.rename(src, dst)
+
+    def get_filename(self, fil):
+        return str.format("{0}_{1}_{2}.fit",
+                fil.get_date().strftime("%Y-%m-%d_%H-%M-%S"),
+                fil.get_fit_sub_type(), fil.get_fit_file_number())
+
+    def get_filepath(self, fil):
+        path = os.path.join(self._device.get_path(),
+                _filetypes[fil.get_fit_sub_type()])
+        return os.path.join(path, self.get_filename(fil))
+
+
+    def download_file(self, fil):
+
+        sys.stdout.write("Downloading " + self.get_filename(fil) + " [")
+        sys.stdout.flush()
+        def callback(new_progress):
+            diff = int(new_progress * 10.0) - int(callback.progress * 10.0)
+            sys.stdout.write("." * diff)
+            sys.stdout.flush()
+            callback.progress = new_progress
+        callback.progress = 0.0
+        data = self.download(fil.get_index(), callback)
+        with open(self.get_filepath(fil), "w") as fd:
+            data.tofile(fd)
+        sys.stdout.write("]\n")
+        sys.stdout.flush()
+
+    def upload_file(self, typ, filename):
+        with open(os.path.join(self._device.get_path(), _filetypes[typ],
+                filename), 'r') as fd:
+            data = array.array('B', fd.read())
+        return self.create(typ, data)
+
+def main():
+
+    # Set up logging
+    _logger.setLevel(logging.DEBUG)
+    handler = logging.FileHandler(AntFSCLI.PRODUCT_NAME + ".log", "w")
+    #handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter(fmt='%(threadName)-10s %(asctime)s  %(name)-15s  %(levelname)-8s  %(message)s'))
+    _logger.addHandler(handler)
+
+    try:
+        g = AntFSCLI()
+        g.start()
+    except Device.ProfileVersionException as e:
+        print "\nError:", str(e), "\n\nThis means that", \
+                AntFSCLI.PRODUCT_NAME, "found that your data directory " \
+                "stucture was too old or too new. The best option is " \
+                "probably to let", AntFSCLI.PRODUCT_NAME, "recreate your " \
+                "folder by deleting your data folder, after backing it up, " \
+                "and let all your files be redownloaded from your sports " \
+                "watch."
+    except (Exception, KeyboardInterrupt) as e:
+        traceback.print_exc()
+        for line in traceback.format_exc().splitlines():
+            _logger.error("%r", line)
+        print "Interrupted:", str(e)
+        g.stop()
+        sys.exit(1)
+
+if __name__ == "__main__":
+    sys.exit(main())
+
